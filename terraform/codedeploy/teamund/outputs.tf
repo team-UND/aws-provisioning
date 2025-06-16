bf7346bba21456ffab7571b3400ca67be736e7b8
choridev <yidodeveloper@gmail.com>
2025-06-17 00:06:33 +0900

    🧱 Set up CodeDeploy for deployment

---
+++ terraform/codedeploy/teamund/outputs.tf
@@ -0,0 +1,7 @@
+output "aws_codedeploy_app_beforegoing_name" {
+  value = aws_codedeploy_app.beforegoing.name
+}
+
+output "aws_codedeploy_deployment_group_beforegoing_deployment_group_name" {
+  value = aws_codedeploy_deployment_group.beforegoing.deployment_group_name
+}
