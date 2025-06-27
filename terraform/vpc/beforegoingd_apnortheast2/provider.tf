191e6023ad4b3ac9422a3daa1e83fe3fe4617f6c
choridev <yidodeveloper@gmail.com>
2025-06-27 13:00:35 +0900

    🔧 Update provider configuration

--- terraform/vpc/beforegoingd_apnortheast2/provider.tf
+++ terraform/vpc/beforegoingd_apnortheast2/provider.tf
@@ -1,9 +1,21 @@
 terraform {
   required_providers {
+
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.95.0"
     }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.1.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5.3"
+    }
+
   }
 }
 
