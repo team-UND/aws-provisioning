8b88e17f78da6fbef0ede57931d0fdcf30b5d221
choridev <yidodevleoper@gmail.com>
2025-07-06 17:00:21 +0900

    🧱 Add S3 resources for Prometheus

--- terraform/s3/teamund/beforegoingd_apnortheast2/codedeploy_server.tf
+++ terraform/s3/teamund/beforegoingd_apnortheast2/codedeploy_server.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "codedeploy_artifact" {
+resource "aws_s3_bucket" "codedeploy_artifact_server" {
   bucket = "codedeploy-artifact-server-${data.terraform_remote_state.vpc.outputs.shard_id}"
 }
 
-resource "aws_s3_bucket_versioning" "codedeploy_artifact" {
-  bucket = aws_s3_bucket.codedeploy_artifact.id
+resource "aws_s3_bucket_versioning" "codedeploy_artifact_server" {
+  bucket = aws_s3_bucket.codedeploy_artifact_server.id
   versioning_configuration {
     status = "Enabled"
   }
--- terraform/s3/teamund/outputs.tf
+++ terraform/s3/teamund/outputs.tf
@@ -1,4 +1,1 @@
-output "codedeploy_artifact_bucket_arn" {
-  description = "ARN of the S3 bucket for CodeDeploy artifacts"
-  value       = aws_s3_bucket.codedeploy_artifact.arn
-}
+
