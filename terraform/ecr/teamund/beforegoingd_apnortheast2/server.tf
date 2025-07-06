fc8b98b03abb78195bae8d7d2d2babcc155a4170
choridev <yidodevleoper@gmail.com>
2025-07-06 16:45:08 +0900

    🚚 Rename ECR repository to beforegoing-server

--- terraform/ecr/teamund/beforegoingd_apnortheast2/server.tf
+++ terraform/ecr/teamund/beforegoingd_apnortheast2/server.tf
@@ -1,5 +1,5 @@
-resource "aws_ecr_repository" "build" {
-  name                 = "beforegoing-build"
+resource "aws_ecr_repository" "server_build" {
+  name                 = "beforegoing-server"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
