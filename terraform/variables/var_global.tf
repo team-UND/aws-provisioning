7b7a7c97df78f861c26342b8cf4b066d76fe62d3
choridev <yidodevleoper@gmail.com>
2025-07-06 19:47:13 +0900

    🔥 Remove unused resources configurations

--- terraform/variables/var_global.tf
+++ terraform/variables/var_global.tf
@@ -63,45 +63,6 @@ variable "remote_state" {
       }
     }
 
-    # ECR
-    ecr = {
-      teamund = {
-
-        beforegoingdapne2 = {
-          region = "ap-northeast-2"
-          bucket = "teamund-apnortheast2-tfstate"
-          key    = "aws-provisioning/terraform/ecr/teamund/beforegoingd_apnortheast2/terraform.tfstate"
-        }
-
-      }
-    }
-
-    # S3
-    s3 = {
-      teamund = {
-
-        beforegoingdapne2 = {
-          region = "ap-northeast-2"
-          bucket = "teamund-apnortheast2-tfstate"
-          key    = "aws-provisioning/terraform/s3/teamund/beforegoingd_apnortheast2/terraform.tfstate"
-        }
-
-      }
-    }
-
-    # CodeDeploy
-    codedeploy = {
-      teamund = {
-
-        beforegoingdapne2 = {
-          region = "ap-northeast-2"
-          bucket = "teamund-apnortheast2-tfstate"
-          key    = "aws-provisioning/terraform/codedeploy/teamund/beforegoingd_apnortheast2/terraform.tfstate"
-        }
-
-      }
-    }
-
     # IAM
     iam = {
       teamund = {
