689051e46383f199d09e62b5dfd47066ca556140
choridev <yidodevleoper@gmail.com>
2025-07-06 16:59:59 +0900

    🚚 Rename S3 resource to server

--- terraform/s3/teamund/beforegoingd_apnortheast2/codedeploy_server.tf
+++ terraform/s3/teamund/beforegoingd_apnortheast2/codedeploy_server.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "codedeploy_artifact" {
-  bucket = "codedeploy-artifact-${data.terraform_remote_state.vpc.outputs.shard_id}"
+  bucket = "codedeploy-artifact-server-${data.terraform_remote_state.vpc.outputs.shard_id}"
 }
 
 resource "aws_s3_bucket_versioning" "codedeploy_artifact" {
