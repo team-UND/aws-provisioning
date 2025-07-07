5910d899ace1e043d1d85777067d613d743f0ea7
choridev <yidodevleoper@gmail.com>
2025-07-07 21:50:34 +0900

    🧱 Update security group references to use ECS

--- terraform/rds/teamund/outputs.tf
+++ terraform/rds/teamund/outputs.tf
@@ -1,3 +1,1 @@
-output "aws_db_master_user_secret_arn" {
-  value = aws_db_instance.beforegoingd_mysql.master_user_secret[0].secret_arn
-}
+
--- terraform/services/server/outputs.tf
+++ terraform/services/server/outputs.tf
@@ -1,3 +1,8 @@
 output "aws_lb_target_group_default_name" {
   value = module.server.aws_lb_target_group_default_name
 }
+
+output "aws_security_group_ecs_tasks_id" {
+  description = "The ID of the security group for the ECS tasks"
+  value       = module.server.aws_security_group_ecs_tasks_id
+}
