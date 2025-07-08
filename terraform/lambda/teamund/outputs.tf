347fe570e798a1ed01c37422b209aded06dd257e
choridev <yidodevleoper@gmail.com>
2025-07-08 20:58:19 +0900

    🔥 Remove obsolete SNS configuration files

--- terraform/lambda/teamund/outputs.tf
+++ terraform/lambda/teamund/outputs.tf
@@ -1,11 +1,3 @@
-output "aws_lambda_function_codedeploy_notification_function_name" {
-  value = aws_lambda_function.codedeploy_notification.function_name
-}
-
-output "aws_lambda_function_codedeploy_notification_arn" {
-  value = aws_lambda_function.codedeploy_notification.arn
-}
-
 output "aws_lambda_function_sentry_notification_invoke_arn" {
   value = aws_lambda_function.sentry_notification.invoke_arn
 }
--- terraform/sns/teamund/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "aws_sns_topic_codedeploy_arn" {
-  description = "The ARN of the SNS topic for CodeDeploy notifications"
-  value       = aws_sns_topic.codedeploy.arn
-}
