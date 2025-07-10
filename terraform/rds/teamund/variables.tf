a61d842d209a70f91a953d51eba29223f71d8770
choridev <yidodevleoper@gmail.com>
2025-07-11 00:46:03 +0900

    🎨 Apply tflint

--- terraform/rds/teamund/variables.tf
+++ terraform/rds/teamund/variables.tf
@@ -1,13 +1,19 @@
 variable "username" {
   description = "Username for DB"
+  type        = string
 }
 
 variable "db_name" {
   description = "Name of DB"
+  type        = string
 }
 
-variable "internal_domain_name" {}
+variable "internal_domain_name" {
+  description = "The internal domain name for the DB"
+  type        = string
+}
 
 variable "port" {
   description = "The port on which the DB accepts connections"
+  type        = string
 }
