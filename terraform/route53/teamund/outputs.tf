b767a472204f72dc8ce9721da75e70c09af4e90b
choridev <yidodevleoper@gmail.com>
2025-07-11 00:42:02 +0900

    🎨 Apply tflint

--- terraform/route53/teamund/outputs.tf
+++ terraform/route53/teamund/outputs.tf
@@ -1,3 +1,4 @@
 output "aws_route53_zone_beforegoing_site_name" {
-  value = aws_route53_zone.beforegoing_site.name
+  description = "The internal domain name for the DB"
+  value       = aws_route53_zone.beforegoing_site.name
 }
--- terraform/route53/teamund/remote_state.tf
+++
@@ -1,8 +0,0 @@
-# Set Configuration to use VPC Information via remote_state
-# VPC backend configuration is located in terraform/variables/var_global.tf
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-
-  # merge function is used to merge the key-value pairs from two different map
-  config = merge(var.remote_state.vpc.beforegoingdapne2)
-}
