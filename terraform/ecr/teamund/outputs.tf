3228862572e69471f61dbc8dbf0c237f0effa037
choridev <yidodevleoper@gmail.com>
2025-07-11 01:03:58 +0900

    🎨 Change the order of resources

--- terraform/ecr/teamund/outputs.tf
+++ terraform/ecr/teamund/outputs.tf
@@ -3,14 +3,14 @@ output "aws_ecr_repository_beforegoingd_server_build_arn" {
   value       = aws_ecr_repository.beforegoingd_server_build.arn
 }
 
-output "aws_ecr_repository_beforegoingd_prometheus_build_arn" {
-  description = "The ARN of the ECR repository for the Prometheus application"
-  value       = aws_ecr_repository.beforegoingd_prometheus_build.arn
-}
-
 output "aws_ecr_repository_beforegoingd_server_build_repository_url" {
   description = "The URL of the ECR repository for the Server application"
   value       = aws_ecr_repository.beforegoingd_server_build.repository_url
+}
+
+output "aws_ecr_repository_beforegoingd_prometheus_build_arn" {
+  description = "The ARN of the ECR repository for the Prometheus application"
+  value       = aws_ecr_repository.beforegoingd_prometheus_build.arn
 }
 
 output "aws_ecr_repository_beforegoingd_prometheus_build_repository_url" {
