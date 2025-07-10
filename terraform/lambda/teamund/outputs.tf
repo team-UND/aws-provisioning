8cbcd15c64b539e88189a6eb6988612b7f2c1b34
choridev <yidodevleoper@gmail.com>
2025-07-10 23:14:09 +0900

    🔥 Remove obsolete Lambda configuration files

--- terraform/lambda/teamund/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "aws_lambda_function_sentry_notification_invoke_arn" {
-  value = aws_lambda_function.sentry_notification.invoke_arn
-}
-
-output "aws_lambda_function_sentry_notification_arn" {
-  value = aws_lambda_function.sentry_notification.arn
-}
--- terraform/lambda/teamund/variablest.tf
+++
@@ -1,5 +0,0 @@
-variable "discord_webhook_url" {
-  description = "Discord Webhook URL for sending notifications."
-  type        = string
-  sensitive   = true
-}
