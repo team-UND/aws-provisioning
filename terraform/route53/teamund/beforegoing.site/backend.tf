2dea583ea0514bbf137310401c1d65901caa86a3
choridev <yidodevleoper@gmail.com>
2025-07-11 12:17:13 +0900

    🎨 Move terraform required version to provider.tf

--- terraform/route53/teamund/beforegoing.site/backend.tf
+++ terraform/route53/teamund/beforegoing.site/backend.tf
@@ -1,6 +1,4 @@
 terraform {
-  required_version = ">= 1.10.5"
-
   backend "s3" {
     bucket         = "teamund-apnortheast2-tfstate"
     key            = "aws-provisioning/terraform/route53/teamund/beforegoing.site/terraform.tfstate"
