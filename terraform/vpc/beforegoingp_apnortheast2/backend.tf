e67a55a992cbb5b849936f6c2a1299ea36e7bb8f
choridev <yidodevleoper@gmail.com>
2025-07-13 21:11:25 +0900

    🔥 Remove obsolete prod env vpc files

--- terraform/vpc/beforegoingp_apnortheast2/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "teamund-apnortheast2-tfstate"
-    key            = "aws-provisioning/terraform/vpc/beforegoingp_apnortheast2/terraform.tfstate"
-    region         = "ap-northeast-2"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
-}
--- terraform/vpc/beforegoingp_apnortheast2/route53.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route53_zone" "internal" {
-  name    = var.internal_domain_name
-  comment = "${var.vpc_name} - Managed by Terraform"
-
-  vpc {
-    vpc_id = aws_vpc.default.id
-  }
-}
