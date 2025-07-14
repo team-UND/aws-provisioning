3b76bc99253e3e4692688bbde21c1aac60b7a9f4
choridev <yidodevleoper@gmail.com>
2025-07-14 23:09:33 +0900

    🧱 Add ECR lifecycle policy

--- terraform/ecr/_module/repository/repository.tf
+++ terraform/ecr/_module/repository/repository.tf
@@ -6,3 +6,13 @@ resource "aws_ecr_repository" "default" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "default" {
+  count      = var.create_lifecycle_policy ? 1 : 0
+  repository = aws_ecr_repository.default.name
+
+  policy = templatefile("${path.module}/ecr_lifecycle_policy.json.tftpl", {
+    untagged_image_expiration_days = var.untagged_image_expiration_days
+    tagged_image_retention_count   = var.tagged_image_retention_count
+  })
+}
--- terraform/ecr/_module/repository/variables.tf
+++ terraform/ecr/_module/repository/variables.tf
@@ -1,4 +1,20 @@
 variable "repository_name" {
-  description = "The name of the repository"
+  description = "The name of the ECR repository"
   type        = string
 }
+
+variable "create_lifecycle_policy" {
+  description = "Whether to create a lifecycle policy for the repository"
+  type        = bool
+}
+
+variable "untagged_image_expiration_days" {
+  description = "Number of days after which to expire untagged images"
+  type        = number
+}
+
+variable "tagged_image_retention_count" {
+  description = "Number of tagged images to retain"
+  type        = number
+}
+
