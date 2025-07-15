7a215d38a33ccd1371a7e7c904a497ee31840161
choridev <yidodevleoper@gmail.com>
2025-07-15 18:22:21 +0900

    🧱 Set up RDS in apnortheast1

---
+++ terraform/rds/teamund/beforegoingd_apnortheast1/mysql.tf
@@ -0,0 +1,31 @@
+module "mysql" {
+  source = "../../_module/mysql"
+
+  vpc_id     = data.terraform_remote_state.vpc.outputs.vpc_id
+  vpc_name   = data.terraform_remote_state.vpc.outputs.vpc_name
+  shard_id   = data.terraform_remote_state.vpc.outputs.shard_id
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_db_subnet_ids
+
+  route53_zone_id = data.terraform_remote_state.vpc.outputs.route53_internal_zone_id
+  domain_name     = "mysql-dev"
+  port            = 3006
+
+  engine_version             = "8.0"
+  auto_minor_version_upgrade = true
+  instance_class             = "db.t3.micro"
+  allocated_storage          = 20
+  max_allocated_storage      = 0
+  storage_type               = "gp2"
+
+  username                        = "team_UND_Beforegoing_admin"
+  db_name                         = "beforegoingd"
+  manage_master_user_password     = true
+  multi_az                        = false
+  publicly_accessible             = false
+  backup_retention_period         = 7
+  copy_tags_to_snapshot           = true
+  enabled_cloudwatch_logs_exports = ["general"]
+  skip_final_snapshot             = true
+  family                          = "mysql8.0"
+  pg_variables                    = var.pg_variables
+}
--- terraform/rds/teamund/variables/var_pg.tf
+++ terraform/rds/teamund/variables/var_pg.tf
@@ -23,6 +23,24 @@ variable "pg_variables" {
 
     parameters = {
 
+      beforegoingdapne1 = [
+        {
+          name         = "time_zone"
+          value        = "Asia/Seoul"
+          apply_method = "pending-reboot"
+        },
+        {
+          name         = "character_set_server"
+          value        = "utf8mb4"
+          apply_method = "pending-reboot"
+        },
+        {
+          name         = "wait_timeout"
+          value        = "1200"
+          apply_method = "immediate"
+        }
+      ],
+
       beforegoingdapne2 = [
         {
           name         = "time_zone"
