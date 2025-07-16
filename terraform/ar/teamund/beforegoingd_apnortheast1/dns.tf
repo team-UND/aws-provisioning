b755402a92c247bddeedc32a0d7769c5892fb715
choridev <yidodevleoper@gmail.com>
2025-07-16 23:30:51 +0900

    🎨 Use terraform remote state instead of var

--- terraform/ar/teamund/beforegoingd_apnortheast1/dns.tf
+++ terraform/ar/teamund/beforegoingd_apnortheast1/dns.tf
@@ -3,6 +3,6 @@ module "dns" {
 
   certificate_validation_records = module.service.aws_apprunner_custom_domain_association_certificate_validation_records
   dns_target                     = module.service.aws_apprunner_custom_domain_association_dns_target
-  route53_zone_id                = var.r53_variables.dev.beforegoing_store_zone_id
+  route53_zone_id                = data.terraform_remote_state.hosting_zone.outputs.aws_route53_zone_id
   subdomain_name                 = var.subdomain_name
 }
