43a057821771ece49848c9734746a413a983cff3
choridev <yidodevleoper@gmail.com>
2025-07-16 23:32:33 +0900

    🎨 Add aws route53 zone id output

--- terraform/route53/_module/hosting_zone/outputs.tf
+++ terraform/route53/_module/hosting_zone/outputs.tf
@@ -1,3 +1,8 @@
+output "aws_route53_zone_id" {
+  description = "The ID of the Route53 zone to create"
+  value       = aws_route53_zone.default.zone_id
+}
+
 output "aws_route53_zone_name" {
   description = "The name of the Route53 zone to create"
   value       = aws_route53_zone.default.name
--- terraform/route53/teamund/outputs.tf
+++ terraform/route53/teamund/outputs.tf
@@ -1,3 +1,8 @@
+output "aws_route53_zone_id" {
+  description = "The ID of the Route53 zone to create"
+  value       = module.hosting_zone.aws_route53_zone_id
+}
+
 output "aws_route53_zone_name" {
   description = "The name of the Route53 zone to create"
   value       = module.hosting_zone.aws_route53_zone_name
