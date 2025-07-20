b9bae7afb64488902db812754ccd4fba79758cd0
choridev <yidodevleoper@gmail.com>
2025-07-21 00:45:26 +0900

    🚚 Change the structure of the directory

--- terraform/rds/teamund/beforegoingd_apnortheast1/outputs.tf
+++ terraform/rds/teamund/beforegoingd_apnortheast1/outputs.tf
@@ -1,1 +1,14 @@
-../outputs.tf
+output "aws_db_master_user_secret_arn" {
+  description = "The ARN of the secret containing the master user password"
+  value       = module.mysql.aws_db_master_user_secret_arn
+}
+
+output "aws_security_group_id" {
+  description = "The ID of the security group for the RDS"
+  value       = module.mysql.aws_security_group_id
+}
+
+output "port" {
+  description = "The port the DB is listening on"
+  value       = module.mysql.port
+}
