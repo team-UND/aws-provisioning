a768834e39d560d94fe0273c7c072043d7a83a10
choridev <yidodevleoper@gmail.com>
2025-07-21 00:23:10 +0900

    🧱 Use state files in beforegoingp apnortheast2

--- terraform/apigw/teamund/beforegoingp_apnortheast2/remote_state.tf
+++ terraform/apigw/teamund/beforegoingp_apnortheast2/remote_state.tf
@@ -1,23 +1,23 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
 
-  config = merge(var.remote_state.vpc.beforegoingdapne2)
+  config = merge(var.remote_state.vpc.beforegoingpapne2)
 }
 
 data "terraform_remote_state" "hosting_zone" {
   backend = "s3"
 
-  config = merge(var.remote_state.route53.hosting_zone.beforegoingdapne2)
+  config = merge(var.remote_state.route53.hosting_zone.beforegoingpapne2)
 }
 
 data "terraform_remote_state" "int_lb" {
   backend = "s3"
 
-  config = merge(var.remote_state.lb.internal.beforegoingdapne2)
+  config = merge(var.remote_state.lb.internal.beforegoingpapne2)
 }
 
 data "terraform_remote_state" "function" {
   backend = "s3"
 
-  config = merge(var.remote_state.lambda.function.beforegoingdapne2)
+  config = merge(var.remote_state.lambda.function.beforegoingpapne2)
 }
