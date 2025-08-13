ad304cd453236aa8f0130ce40e296495c1cd3940
Chori <yidodeveloper@gmail.com>
2025-08-13 10:11:00 +0900

    🧱 Add API GW outputs on apne1 for dev

---
+++ terraform/apigw/teamund/beforegoingd_apnortheast1/outputs.tf
@@ -0,0 +1,14 @@
+output "aws_apigatewayv2_api_id" {
+  description = "The ID of the API Gateway V2 HTTP API"
+  value       = module.http.aws_apigatewayv2_api_id
+}
+
+output "aws_apigatewayv2_api_endpoint" {
+  description = "The endpoint of the API Gateway to be used as an origin for CloudFront"
+  value       = module.http.aws_apigatewayv2_api_endpoint
+}
+
+output "aws_apigatewayv2_stage_name" {
+  description = "The name of the API Gateway stage"
+  value       = module.http.aws_apigatewayv2_stage_name
+}
