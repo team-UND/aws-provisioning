a400daf8a403be07d3f8b57d47e28bc150f4bd96
Chori <yidodeveloper@gmail.com>
2025-08-13 16:26:45 +0900

    🧱 Create authorizer Lambda function

---
+++ terraform/iam/teamund/beforegoingd_apnortheast1/authorizer_role.tf
@@ -0,0 +1,56 @@
+data "aws_secretsmanager_secret" "origin_verify" {
+  provider = aws.vpc_region
+  name     = "dev/origin-verify/cloudfront"
+}
+
+resource "aws_iam_role" "authorizer" {
+  description = "Role for Authorizer Lambda function"
+  name        = "authorizer-${data.terraform_remote_state.vpc.outputs.vpc_name}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name        = "authorizer-${data.terraform_remote_state.vpc.outputs.vpc_name}"
+    Environment = data.terraform_remote_state.vpc.outputs.billing_tag
+  }
+}
+
+resource "aws_iam_policy" "authorizer_secrets_read" {
+  description = "Policy for Authorizer Lambda function to read secrets from Secrets Manager"
+  name        = "authorizer-secrets-read-${data.terraform_remote_state.vpc.outputs.vpc_name}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "secretsmanager:GetSecretValue"
+        Resource = [
+          # Add secret ARNs here
+          data.aws_secretsmanager_secret.origin_verify.arn
+        ]
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "authorizer_execution" {
+  role       = aws_iam_role.authorizer.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "authorizer_secrets_read" {
+  role       = aws_iam_role.authorizer.name
+  policy_arn = aws_iam_policy.authorizer_secrets_read.arn
+}
--- terraform/iam/teamund/beforegoingd_apnortheast1/outputs.tf
+++ terraform/iam/teamund/beforegoingd_apnortheast1/outputs.tf
@@ -7,3 +7,8 @@ output "aws_iam_role_ar_instance_arn" {
   description = "ARN of the IAM role for App Runner instance"
   value       = aws_iam_role.ar_instance.arn
 }
+
+output "aws_iam_role_authorizer_arn" {
+  description = "ARN of the IAM role for Authorizer Lambda function"
+  value       = aws_iam_role.authorizer.arn
+}
