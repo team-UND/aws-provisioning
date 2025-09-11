923adb7afcf12718811486c9b0236014e03db28d
Chori <yidodeveloper@gmail.com>
2025-09-11 18:02:02 +0900

    🧱 Set up Cloudfront for stg

---
+++ terraform/cloudfront/teamund/beforegoings_apnortheast2/remote_state.tf
@@ -0,0 +1,35 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = merge(var.remote_state.vpc.beforegoingsapne2)
+}
+
+data "terraform_remote_state" "hosting_zone" {
+  backend = "s3"
+
+  config = merge(var.remote_state.route53.hosting_zone.beforegoingsapne2)
+}
+
+data "terraform_remote_state" "bucket" {
+  backend = "s3"
+
+  config = merge(var.remote_state.s3.bucket.beforegoingsapne2)
+}
+
+data "terraform_remote_state" "acl" {
+  backend = "s3"
+
+  config = merge(var.remote_state.waf.acl.beforegoingsapne2)
+}
+
+data "terraform_remote_state" "function" {
+  backend = "s3"
+
+  config = merge(var.remote_state.lambda.function.beforegoingsapne2)
+}
+
+data "terraform_remote_state" "apigw" {
+  backend = "s3"
+
+  config = merge(var.remote_state.apigw.http.beforegoingsapne2)
+}
