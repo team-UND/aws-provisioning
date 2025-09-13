d3b0d2c38ee1cd6579cd4cd4d4424aa41f700029
Chori <yidodeveloper@gmail.com>
2025-09-13 17:29:22 +0900

    🧱 Delete DynamoDB for terraform state lock

--- terraform/init/_module/local/local.tf
+++ terraform/init/_module/local/local.tf
@@ -14,15 +14,3 @@ resource "aws_s3_bucket_versioning" "tfstate_versioning" {
     status = "Enabled"
   }
 }
-
-# DynamoDB for terraform state lock
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name         = "terraform-lock"
-  hash_key     = "LockID"
-  billing_mode = "PAY_PER_REQUEST"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
