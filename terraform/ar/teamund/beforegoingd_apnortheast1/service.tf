cbbb0fd77608a38d44c791604a0a05ed3820599b
Chori <yidodeveloper@gmail.com>
2025-09-17 14:47:33 +0900

    🚚 Change image tag to use to ae6fc87

--- terraform/ar/teamund/beforegoingd_apnortheast1/service.tf
+++ terraform/ar/teamund/beforegoingd_apnortheast1/service.tf
@@ -36,7 +36,7 @@ module "service" {
   shard_id = data.terraform_remote_state.vpc.outputs.shard_id
 
   image_url             = data.terraform_remote_state.repository.outputs.aws_ecr_repository_server_build_repository_url
-  image_tag             = "f49466d"
+  image_tag             = "ae6fc87"
   image_repository_type = "ECR"
 
   domain_name          = data.terraform_remote_state.hosting_zone.outputs.aws_route53_zone_name
